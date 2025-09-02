18134ac84645383326d385059f65932afcf9e014
aditya-7-7 <136198786+aditya-7-7@users.noreply.github.com>
2025-09-03 03:47:19 +0530

    Update performance-analysis.sql

--- scripts/analytics/performance-analysis.sql
+++ scripts/analytics/performance-analysis.sql
@@ -5,11 +5,6 @@ Purpose:
     - To measure the performance of products, customers, or regions over time.
     - For benchmarking and identifying high-performing entities.
     - To track yearly trends and growth.
-
-SQL Functions Used:
-    - LAG(): Accesses data from previous rows.
-    - AVG() OVER(): Computes average values within partitions.
-    - CASE: Defines conditional logic for trend analysis.
 ===============================================================================
 */
 
@@ -48,4 +43,5 @@ SELECT
         ELSE 'No Change'
     END AS py_change
 FROM yearly_product_sales
+
 ORDER BY product_name, order_year;
