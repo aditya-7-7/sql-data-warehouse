043e02a4d2dc621a673180287278734689c97222
aditya-7-7 <136198786+aditya-7-7@users.noreply.github.com>
2025-09-03 03:47:31 +0530

    Update ranking-analysis.sql

--- scripts/analytics/ranking-analysis.sql
+++ scripts/analytics/ranking-analysis.sql
@@ -4,10 +4,6 @@ Ranking Analysis
 Purpose:
     - To rank items (e.g., products, customers) based on performance or other metrics.
     - To identify top performers or laggards.
-
-SQL Functions Used:
-    - Window Ranking Functions: RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
-    - Clauses: GROUP BY, ORDER BY
 ===============================================================================
 */
 
@@ -74,4 +70,5 @@ GROUP BY
     c.customer_key,
     c.first_name,
     c.last_name
+
 ORDER BY total_orders ;
